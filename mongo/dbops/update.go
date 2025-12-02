3cfabd743f8c61bb2017c31af1e0f026648ef680
Moinak Dey <moinak.dey8@gmail.com>
2025-12-02 17:04:42 +0530

    context related bugfix

--- mongo/dbops/update.go
+++ mongo/dbops/update.go
@@ -1,6 +1,7 @@
 package dbops
 
 import (
+	"context"
 	"fmt"
 	"ots/model"
 	"ots/settings"
@@ -14,7 +15,9 @@ import (
 )
 
 func AssignResolverToTicket(ticket *model.Ticket, resolverId primitive.ObjectID) (*model.Ticket, error) {
-	// defer settings.MySettings.Get_CtxCancel()()
+	ctxbase := context.TODO()
+	ctx, cancel := context.WithTimeout(ctxbase, settings.MySettings.Get_CtxTimeout())
+	defer cancel()
 
 	coll := mgm.Coll(ticket)
 
@@ -29,7 +32,7 @@ func AssignResolverToTicket(ticket *model.Ticket, resolverId primitive.ObjectID)
 	}
 
 	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
-	if err := coll.FindOneAndUpdate(settings.MySettings.Get_CtxWithTimeout(), filter, update, opts).Decode(&ticket); err != nil {
+	if err := coll.FindOneAndUpdate(ctx, filter, update, opts).Decode(&ticket); err != nil {
 		// ticket does not exist
 		return nil, fmt.Errorf("ticket with ID - %s does not exist: %v", ticket.ID, err)
 	}
@@ -38,6 +41,10 @@ func AssignResolverToTicket(ticket *model.Ticket, resolverId primitive.ObjectID)
 }
 
 func UpdateTicketStatus(status string, ticketId primitive.ObjectID) error {
+	ctxbase := context.TODO()
+	ctx, cancel := context.WithTimeout(ctxbase, settings.MySettings.Get_CtxTimeout())
+	defer cancel()
+
 	ts := ticketstructs.GenerateTicketStatus()
 	if !ts.IsValidStatus(status) {
 		return fmt.Errorf("invalid ticket status to update")
@@ -58,7 +65,7 @@ func UpdateTicketStatus(status string, ticketId primitive.ObjectID) error {
 	}
 
 	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
-	err := coll.FindOneAndUpdate(settings.MySettings.Get_CtxWithTimeout(), searchfilter, updatefilter, opts).Decode(&ticket)
+	err := coll.FindOneAndUpdate(ctx, searchfilter, updatefilter, opts).Decode(&ticket)
 	if err != nil {
 		return fmt.Errorf("error updating ticket status")
 	}
@@ -67,6 +74,10 @@ func UpdateTicketStatus(status string, ticketId primitive.ObjectID) error {
 }
 
 func AppendTicketMileStone(milestone *model.TicketMilestone, ticketId primitive.ObjectID) error {
+	ctxbase := context.TODO()
+	ctx, cancel := context.WithTimeout(ctxbase, settings.MySettings.Get_CtxTimeout())
+	defer cancel()
+
 	ticket := &model.Ticket{}
 	coll := mgm.Coll(ticket)
 
@@ -81,7 +92,7 @@ func AppendTicketMileStone(milestone *model.TicketMilestone, ticketId primitive.
 	}
 
 	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
-	if err := coll.FindOneAndUpdate(settings.MySettings.Get_CtxWithTimeout(), searchExpr, updateExpr, opts).Decode(&ticket); err != nil {
+	if err := coll.FindOneAndUpdate(ctx, searchExpr, updateExpr, opts).Decode(&ticket); err != nil {
 		return fmt.Errorf("error appending milestone to ticket: %v", err)
 	}
 
