158eee34db82fdb14e22b9084f0c0a498c5191a8
Moinak Dey <moinak.dey8@gmail.com>
2025-12-04 10:36:36 +0530

    add cors policy

--- api/REST/server/server.go
+++ api/REST/server/server.go
@@ -5,12 +5,26 @@ import (
 	"log"
 	"ots/api/REST/server/routes"
 	"ots/settings"
+	"ots/util"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Start() {
+	if !util.InDevMode() {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	r := gin.Default()
+
+	// Cors setup
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     settings.MySettings.Get_AllowedClientOrigins(),
+		AllowCredentials: true,
+		AllowHeaders:     []string{"Authorization", "Origin", "Content-Type"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "OPTIONS"},
+	}))
 
 	// Map routes
 	routes.MapRoutes(r)
